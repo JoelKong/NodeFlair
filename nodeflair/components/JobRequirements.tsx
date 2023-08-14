fa51f8222506b83a6716bb24817fd5ccad8d4b2d
Joel Kong <joelkong2001@gmail.com>
2023-08-14 16:12:36 +0800

    feat: finished simplified job desc component

---
+++ nodeflair/components/JobRequirements.tsx
@@ -0,0 +1,3 @@
+export default function JobRequirements(): JSX.Element {
+  return <div>df</div>;
+}
--- nodeflair/components/SimplifiedJobDesc.tsx
+++ nodeflair/components/SimplifiedJobDesc.tsx
@@ -6,20 +6,64 @@ export default function SimplifiedJobDesc(): JSX.Element {
     <>
       {db.map((jobs) => {
         return (
-          <div className="w-full lg:w-1/3 h-32 border-[3px] border-green-500 bg-white rounded-lg p-4 mt-4 flex flex-row">
-            <div>
-              <Image
-                src={`/${jobs.logo}`}
-                alt={jobs.company}
-                width={50}
-                height={50}
-              />
+          <section className="flex flex-col w-full lg:w-[calc(50%-4rem)] relative h-fit border-[3px] border-green-500 bg-white rounded-lg p-4 mt-4 whitespace-nowrap overflow-hidden">
+            <div className="flex flex-col sm:flex-row">
+              <div>
+                <Image
+                  src={`/${jobs.logo}`}
+                  alt={jobs.company}
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <section className="flex flex-col w-5/6 sm:pl-3 sm:pt-0 pt-2">
+                <div className="flex flex-row font-normal">
+                  <p>{jobs.company}</p>
+                  <span className="pl-5">{`${jobs.star} ★`}</span>
+                </div>
+                <p className="font-bold">{jobs.title}</p>
+                <div className="flex flex-row">
+                  <p className="font-bold text-green-400 pr-4">{jobs.posted}</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="25"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="gray"
+                      d="M256 32C167.67 32 96 96.51 96 176c0 128 160 304 160 304s160-176 160-304c0-79.49-71.67-144-160-144Zm0 224a64 64 0 1 1 64-64a64.07 64.07 0 0 1-64 64Z"
+                    />
+                  </svg>
+                  <p className="text-gray-500">{jobs.location}</p>
+                </div>
+                <div className="flex flex-row">
+                  <p className="font-semibold pr-2">{jobs.salary}</p>
+                  <div className="rounded-md text-center bg-gray-200 w-10">
+                    <p className="text-gray-600">EST</p>
+                  </div>
+                </div>
+              </section>
+              <div className="sm:relative absolute right-4">
+                <p className="font-semibold text-green-600 bg-green-200 rounded-md pt-1 pb-1 pl-2 pr-2">
+                  {jobs.category}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-row pl-3 w-4/6">
-              <p>{jobs.company}</p>
-              <span>{jobs.star}</span>
+            <div className="w-full border-gray-100 border-2 mt-5"></div>
+            <div className="flex flex-row">
+              {jobs.requirements.map((requirement, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="mr-2 mt-1 pt-1 pb-1 pl-2 pr-2 rounded-md bg-gray-200 text-gray-500 font-semibold"
+                  >
+                    <span>{requirement}</span>
+                  </div>
+                );
+              })}
             </div>
-          </div>
+          </section>
         );
       })}
     </>
